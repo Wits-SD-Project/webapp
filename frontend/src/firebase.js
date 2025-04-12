c4729a3910f47aa897e7cc8655afd36f9b705576
ibram-chilu <ibramchilufya@gmail.com>
2025-04-12 23:17:32 +0200

    initial commit for authentication (didnt change Sachin's code as it is linked to the User Access of the Web App

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,18 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Ibram replace with your config
 const firebaseConfig = {
-  apiKey: "AIzaSyA_MjCm81gJVDzVav0kL_cgkzpdDEhmLBc",
-  authDomain: "sportssphere-c2046.firebaseapp.com",
-  projectId: "sportssphere-c2046",
-  storageBucket: "sportssphere-c2046.firebasestorage.app",
-  messagingSenderId: "295076511537",
-  appId: "1:295076511537:web:4578290d4431b09218fa10",
-  measurementId: "G-3YF5S1ZXTC",
+  apiKey: "AIzaSyAhEqXvmStTF2_IA7-fAxT074w27ZTubmA",
+  authDomain: "sportssphere-58736.firebaseapp.com",
+  projectId: "sportssphere-58736",
+  storageBucket: "sportssphere-58736.firebasestorage.app",
+  messagingSenderId: "575568726090",
+  appId: "1:575568726090:web:6d92dbc812222646c3beb3"
 };
 
+//this is to initialise our auth
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export const auth = getAuth(app);
+export {app, auth};
+
 export const db = getFirestore(app);
