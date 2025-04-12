659c700fb150dc25d31ffaa697efc663f6238b05
ibram-chilu <ibramchilufya@gmail.com>
2025-04-12 19:19:32 +0200

    first commit (ibram): firebase config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Ibram replace with your config
 const firebaseConfig = {
-  apiKey: "AIzaSyA_MjCm81gJVDzVav0kL_cgkzpdDEhmLBc",
-  authDomain: "sportssphere-c2046.firebaseapp.com",
-  projectId: "sportssphere-c2046",
-  storageBucket: "sportssphere-c2046.firebasestorage.app",
-  messagingSenderId: "295076511537",
-  appId: "1:295076511537:web:4578290d4431b09218fa10",
-  measurementId: "G-3YF5S1ZXTC",
+  apiKey: "AIzaSyAhEqXvmStTF2_IA7-fAxT074w27ZTubmA",
+  authDomain: "sportssphere-58736.firebaseapp.com",
+  projectId: "sportssphere-58736",
+  storageBucket: "sportssphere-58736.firebasestorage.app",
+  messagingSenderId: "575568726090",
+  appId: "1:575568726090:web:6d92dbc812222646c3beb3"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
