52f369b0ee1eadc222afca3439c522f0e1a867be
sachin2911 <sachinmohan29@gmail.com>
2025-04-13 21:38:49 +0200

    Added PORT

--- server/index.js
+++ server/index.js
@@ -22,5 +22,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 
 // Use environment PORT variable for Azure compatibility
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
