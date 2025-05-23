99c958473d16143eca937111adf7fceda034d142
sachin2911 <sachinmohan29@gmail.com>
2025-05-23 18:58:25 +0200

    Added service acc details to the .env

--- server/firebase.js
+++ server/firebase.js
@@ -2,19 +2,8 @@ const { getFirestore } = require("firebase-admin/firestore");
 require("dotenv").config();
 const admin = require("firebase-admin");
 
-let serviceAccount;
-
 try {
-  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-    // Production (Azure): Load from env
-    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-  } else if (process.env.FIREBASE_KEY_PATH) {
-    // Local dev: Load from file path in .env
-    serviceAccount = require(process.env.FIREBASE_KEY_PATH);
-  } else {
-    throw new Error("No Firebase service account credentials found.");
-  }
-  // Test
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
